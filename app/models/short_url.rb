c382f8bbb87cd80bd923209bf328e79823e61761
kevindsteeleii <anubis523@gmail.com>
2021-09-26 10:44:27 -0400

    method #self.find_by_short_code written w/ add'l helper methods to: a) created decoding look-up hash for base62 -> base10 digits & b) method that uses this hash to convert short_code to original id; corresponding rspec tests passing;

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,11 +5,6 @@ class ShortUrl < ApplicationRecord
   validate :validate_full_url
 
   def short_code
-    # generate short code using "CHARACTERS" array
-    # 1. check to see if there's an id/ object's been saved...
-    # 2. using the non-repeating, unique attribute id (INTEGER) of each short_url, convert from base10 to base62
-    # 3. iterate and map through the array of base62 converted "digits" mapping each one to corresponding character from "CHARACTERS" array matching the base62 digit with the character at the same index
-    # 4. join array as a string, urls can't be enumerables/arrays...
     if self.id.present? 
       converted_id_array = self.id.digits(62) # convert id from base10 to base62
       converted_id_array.map{ |digit| CHARACTERS[digit] }.reverse.join 
@@ -19,9 +14,41 @@ class ShortUrl < ApplicationRecord
   def update_title!
   end
 
+  def self.find_by_short_code(short_code) 
+    # 1. decode shortened code -> id, use id to fetch short_url
+    # 2. create/invoke helper method #self.decoder_hash that inverts the mapping from #short_code by creating a hash w/ key-value pairs of base62_digit and it's index in CHARACTERS array and memoizes it...
+      # 2a. see private method #self.decoder_hash
+    # 3. create/invoke helper method that does the conversion of short_code back to id, returns the id see method #self.convert_short_code_to_id
+    # 4. use returned instance_id to look up corresponding short_url
+    # 5. error handling TBD...
+    instance_id = self.convert_short_code_to_id(short_code)
+    ShortUrl.find(instance_id)
+  end
+
   private
 
   def validate_full_url
+  end
+
+  def self.decoder_hash 
+    @decoder_hash ||= CHARACTERS.each_with_index.map { |value, index| [value, index] }.to_h
+  end
+
+  def self.convert_short_code_to_id(short_code) 
+    # 3a. make a variable with a value of 0 (base10), called instance_id
+    # 3b. revert short_code back to its array of base62 digits, taking care to reverse it as well since originally it was in descending not ascending order...
+    # 3c. iterate through array of base62 digits, making sure to have the current index available in the block
+    # 3d. per loop increment variable instance_id by the following formula: 62**index * decoding_hash_value_at_address_base62_digit
+    # 3e. it follows the same logic as this base10 example: 104 => 10**2 * 1 + 10**1 * 0 + 10**0 * 4, no need to use parens PEMDAS!!!
+    instance_id = 0
+    split_short_code = short_code.split("").reverse
+
+    split_short_code.each_with_index do |value, index| 
+      current_digit = 62**index * self.decoder_hash[value]
+      instance_id += current_digit
+    end
+
+    instance_id
   end
 
 end

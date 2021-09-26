20b102315efe84de08407bb79539c0bd2e1f7549
kevindsteeleii <anubis523@gmail.com>
2021-09-26 11:31:32 -0400

    ShorShortUrlsController#index created; matches initial rspec test;

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,6 +4,14 @@ class ShortUrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    # 1. query short_urls that match the description (top 100 click_count, descending short_urls)
+    # 2. map query to #short_code and assign to @short_codes variable
+    # 3. render @short_codes as a json with structure that matches Rspec test
+    @short_codes = ShortUrl.order(click_count: :desc).limit(100).map do |short_url| 
+      short_url.short_code
+    end
+
+    render json: {urls: @short_codes}
   end
 
   def create

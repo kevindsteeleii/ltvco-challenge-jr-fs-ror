3594f6e8b3b78f2cf2c512c715f157d0e6e74ffd
kevindsteeleii <anubis523@gmail.com>
2021-09-26 12:06:07 -0400

    ShortUrlsController#show tests passing, wrote a rescue for the ActiveRecord::RecordNotFound error; #show tests passed;

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -2,6 +2,7 @@ class ShortUrlsController < ApplicationController
 
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
     @short_codes = ShortUrl.order(click_count: :desc).limit(100).map do |short_url| 
@@ -23,9 +24,21 @@ class ShortUrlsController < ApplicationController
   end
 
   def show
+    @short_url = ShortUrl.find_by_short_code(params[:id])
+
+    @short_url.tap do |short_url| 
+      short_url.click_count += 1
+    end
+
+    @short_url.save!
+    redirect_to @short_url.full_url
   end
 
   private
+
+  def record_not_found 
+    render plain: "404 Not Found", status: 404
+  end
 
   def short_url_params 
     params.permit(:full_url, :id)

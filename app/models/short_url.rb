8118adf6b233e7786007de420437678834d29595
kevindsteeleii <anubis523@gmail.com>
2021-09-26 09:59:20 -0400

    test revealed that the array of base62 digits was in descending order, so I reversed and passed corresponding test;

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,6 +5,15 @@ class ShortUrl < ApplicationRecord
   validate :validate_full_url
 
   def short_code
+    # generate short code using "CHARACTERS" array
+    # 1. check to see if there's an id/ object's been saved...
+    # 2. using the non-repeating, unique attribute id (INTEGER) of each short_url, convert from base10 to base62
+    # 3. iterate and map through the array of base62 converted "digits" mapping each one to corresponding character from "CHARACTERS" array matching the base62 digit with the character at the same index
+    # 4. join array as a string, urls can't be enumerables/arrays...
+    if self.id.present? 
+      converted_id_array = self.id.digits(62) # convert id from base10 to base62
+      converted_id_array.map{ |digit| CHARACTERS[digit] }.reverse.join 
+    end
   end
 
   def update_title!
